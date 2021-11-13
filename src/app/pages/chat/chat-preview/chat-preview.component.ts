689863cee7e8c94722a693703a1ab7d60e0cfbdd
Alexander Shatoba <shatoba97@gmail.com>
2021-11-14 01:07:42 +0300

    Add chat page

--- src/app/pages/chat/chat-preview/chat-preview.component.ts
+++ src/app/pages/chat/chat-preview/chat-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chat-preview',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatPreviewComponent implements OnInit {
 
+  @Input()
+  public chat = null;
   constructor() { }
 
   ngOnInit(): void {
