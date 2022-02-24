a55fbbeb13e3d65a4633d6c9d4056fa41ae37186
Aleksandr Shatoba <shatoba97@gmail.com>
2022-02-24 20:25:16 +0300

    TC-4 Add sidenav

--- src/app/pages/chat/chat.module.ts
+++ src/app/pages/chat/chat.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatComponent } from './chat.component';
 import { ChatPreviewComponent } from './chat-preview/chat-preview.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
@@ -11,7 +12,8 @@ import { ChatPreviewComponent } from './chat-preview/chat-preview.component';
     ChatPreviewComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class ChatModule { }
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   imports: [
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSidenavModule
   ],
   exports: [
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSidenavModule
   ]
 })
 export class MaterialModule { }
